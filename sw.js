ea069ae47a86d652d09d8b3e53bb49bf5179596f
Luke.720 <jingxi1074@gmail.com>
2026-09-10 16:51:17 +0800

    Rev.14
    Debug

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for Luke旅遊紀錄 PWA
-const CACHE_NAME = 'travel-luke-cache-v9';
+﻿// Service Worker for Luke旅遊紀錄 PWA
+const CACHE_NAME = 'travel-luke-cache-v10';
 const STATIC_ASSETS = [
   './',
   './index.html',
